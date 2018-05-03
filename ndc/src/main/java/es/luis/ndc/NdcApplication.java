36a38f5ca83a63d576307e3c9076e470026080a5
LUIS ORDONEZ COSTA <0008017@insags.com>
2018-05-03 15:19:05 +0200

    new file:   ndc/.gitignore 	new file:   ndc/.mvn/wrapper/maven-wrapper.jar 	new file:   ndc/.mvn/wrapper/maven-wrapper.properties 	new file:   ndc/mvnw 	new file:   ndc/mvnw.cmd 	new file:   ndc/pom.xml 	new file:   ndc/src/main/java/es/luis/ndc/NdcApplication.java 	new file:   ndc/src/main/resources/application.properties 	new file:   ndc/src/test/java/es/luis/ndc/NdcApplicationTests.java

---
+++ ndc/src/main/java/es/luis/ndc/NdcApplication.java
@@ -0,0 +1,12 @@
+package es.luis.ndc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NdcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NdcApplication.class, args);
+	}
+}
